fb62605c67b83160ad57ef0ad5bb22ffe978c52c
Beven Nyamande <bevennyamande@gmail.com>
2020-10-08 13:43:21 +0200

    files

--- scrapper.py
+++ scrapper.py
@@ -2,9 +2,12 @@ import os
 from datetime import date
 import pandas as pd
 import git
+from git import Git
 
 URL = "https://www.zse.co.zw/price-sheet/"
-# REPO_URL = "https://github.com/bevennyamande/Scrapper.git"
+
+git_ssh_identity_file = os.path.expanduser('~/.ssh/id_rsa')
+git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
 
 def get_todays_pricesheet(URL: str):
     """ Get the price sheet from the website """
@@ -30,10 +33,20 @@ def is_file_exist():
     """ Check if the file already exists """
     pass
 
+def update_repo():
+    with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
+        repo = git.Repo(".")
+        repo.git.add("--all")
+        repo.git.commit("-m files")
+        git.push()
+    return
+
+
 
 def main():
     df = get_todays_pricesheet(URL)
     df[0].to_excel(set_todays_filename(), header=True, index=False)
+    update_repo()
 
 if __name__ == "__main__":
     main()
